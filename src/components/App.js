98ad359648390d83beb516d057679643d6bfe0ba
Kevin <kevinqiyf@hotmail.com>
2019-10-10 14:25:09 -0700

    add router and refactor App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import Palette from './Palette';
 import seekPalettes from '../data/seedPalettes';
 import { generatePalette } from '../utils/ColorHelper';
 
+const findPalette = id => {
+  return seekPalettes.find(function(palette) {
+    return palette.id === id;
+  });
+};
+
 function App() {
   return (
     <div className="App">
-      <Palette palette={generatePalette(seekPalettes[1])} />
+      <Switch>
+        <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(findPalette(routeProps.match.params.id))}
+            />
+          )}
+        />
+      </Switch>
+
+      {/* <Palette palette={generatePalette(seekPalettes[1])} /> */}
     </div>
   );
 }
---
+++ src/components/PaletteList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PaletteList = props => {
+  return <div>palettelist</div>;
+};
+
+PaletteList.propTypes = {};
+
+export default PaletteList;
