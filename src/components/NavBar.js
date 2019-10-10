db4cbe72644fba5b182f57b58ab1fb17397a4e22
Kevin <kevinqiyf@hotmail.com>
2019-10-10 13:05:30 -0700

    add snackbar and style select

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Slider from 'rc-slider';
-import { Select, MenuItem } from '@material-ui/core';
+import { Select, MenuItem, Snackbar, IconButton } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 
 import 'rc-slider/assets/index.css';
 import '../styles/NavBar.css';
 
 const NavBar = ({ level, setLevel, format, setFormat }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleFormatChanged = e => {
+    setFormat(e.target.value);
+    setOpen(true);
+  };
+
   return (
     <header className="Navbar">
       <div className="logo">
@@ -25,12 +33,36 @@ const NavBar = ({ level, setLevel, format, setFormat }) => {
         </div>
       </div>
       <div className="select-container">
-        <Select value={format} onChange={e => setFormat(e.target.value)}>
+        <Select value={format} onChange={handleFormatChanged}>
           <MenuItem value="hex">HEX - #ffffff</MenuItem>
           <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
           <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0)</MenuItem>
         </Select>
       </div>
+
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left'
+        }}
+        open={open}
+        autoHideDuration={3000}
+        onClose={() => setOpen(false)}
+        ContentProps={{
+          'aria-describedby': 'message-id'
+        }}
+        message={<span id="message-id">Format Changed!</span>}
+        action={[
+          <IconButton
+            key="close"
+            aria-label="close"
+            color="inherit"
+            onClick={() => setOpen(false)}
+          >
+            <CloseIcon />
+          </IconButton>
+        ]}
+      />
     </header>
   );
 };
