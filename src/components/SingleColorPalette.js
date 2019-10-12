c233d40270310ad53d5d81500a08ec53d4fb83a2
Kevin <kevinqiyf@hotmail.com>
2019-10-12 12:16:05 -0700

    add go back button

--- src/components/SingleColorPalette.js
+++ src/components/SingleColorPalette.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { gatherShades } from '../utils/ColorHelper';
+import { Link } from 'react-router-dom';
 import ColorBox from './ColorBox';
 import NavBar from './NavBar';
 import PaletteFooter from './PaletteFooter';
@@ -8,7 +9,7 @@ import PaletteFooter from './PaletteFooter';
 const SingleColorPalette = ({ colorId, palette }) => {
   const [format, setFormat] = useState('hex');
 
-  const { paletteName, emoji } = palette;
+  const { paletteName, emoji, id } = palette;
 
   const colorShades = gatherShades(palette, colorId);
   const colorBoxes = colorShades.map(color => (
@@ -21,9 +22,16 @@ const SingleColorPalette = ({ colorId, palette }) => {
   ));
 
   return (
-    <div className="Palette">
+    <div className="SingleColorPalette Palette">
       <NavBar format={format} setFormat={setFormat} showingAllColors={false} />
-      <div className="Palette-colors">{colorBoxes}</div>
+      <div className="Palette-colors">
+        {colorBoxes}
+        <div className="go-back ColorBox">
+          <Link to={`/palette/${id}`} className="back-button">
+            Go Back
+          </Link>
+        </div>
+      </div>
       <PaletteFooter paletteName={paletteName} emoji={emoji} />
     </div>
   );
