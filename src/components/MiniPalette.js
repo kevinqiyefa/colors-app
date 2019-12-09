b108268baa536a808406a132eee1d307d466bed6
Kevin <kevinqiyf@hotmail.com>
2019-12-09 14:35:45 -0800

    Add deleteIcon to MiniPalette and move out the mini palette css

--- src/components/MiniPalette.js
+++ src/components/MiniPalette.js
@@ -1,47 +1,10 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/styles';
 import PropTypes from 'prop-types';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+import useStyles from '../styles/MiniPaletteStyles';
 
 const MiniPalette = ({ colors, paletteName, emoji, handleClick }) => {
-  const useStyles = makeStyles({
-    root: {
-      backgroundColor: 'white',
-      border: '1px solid black',
-      borderRadius: '5px',
-      padding: '0.5rem',
-      position: 'relative',
-      overflow: 'hidden',
-      cursor: 'pointer'
-    },
-    colors: {
-      backgroundColor: '#dae1e4',
-      height: '100px',
-      width: '100%',
-      borderRadius: '5px',
-      overflow: 'hidden'
-    },
-    title: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      margin: '0',
-      color: 'black',
-      paddingTop: '0.5rem',
-      fontSize: '1rem',
-      position: 'relative'
-    },
-    emoji: { marginLeft: '0.5rem', fontSize: '1.5rem' },
-
-    miniColor: {
-      height: '25%',
-      width: '20%',
-      display: 'inline-block',
-      margin: '0 auto',
-      position: 'relative',
-      marginBottom: '-3.5px'
-    }
-  });
-
   const classes = useStyles();
 
   const miniColorBoxes = colors.map(color => (
@@ -54,6 +17,12 @@ const MiniPalette = ({ colors, paletteName, emoji, handleClick }) => {
 
   return (
     <div className={classes.root} onClick={handleClick}>
+      <div className={classes.delete}>
+        <DeleteIcon
+          className={classes.deleteIcon}
+          style={{ transition: 'all 0.3s ease-in-out' }}
+        />
+      </div>
       <div className={classes.colors}>{miniColorBoxes}</div>
       <h5 className={classes.title}>
         {paletteName} <span className={classes.emoji}>{emoji}</span>
