81a8917a73801abd06df2643e3c839d16418e3d5
Kevin <kevinqiyf@hotmail.com>
2019-12-11 16:41:29 -0800

    Make PaletteList responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,4 +1,5 @@
 import { makeStyles } from '@material-ui/styles';
+import sizes from './sizes';
 
 export default makeStyles({
   root: {
@@ -13,7 +14,13 @@ export default makeStyles({
     display: 'flex',
     alignItems: 'flex-start',
     flexDirection: 'column',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
+    [sizes.down('xl')]: {
+      width: '80%'
+    },
+    [sizes.down('xs')]: {
+      width: '75%'
+    }
   },
   nav: {
     display: 'flex',
@@ -30,6 +37,13 @@ export default makeStyles({
     width: '100%',
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 30%)',
-    gridGap: '5%'
+    gridGap: '2.5rem',
+    [sizes.down('md')]: {
+      gridTemplateColumns: 'repeat(2, 50%)'
+    },
+    [sizes.down('xs')]: {
+      gridTemplateColumns: 'repeat(1, 100%)',
+      gridGap: '1rem'
+    }
   }
 });
